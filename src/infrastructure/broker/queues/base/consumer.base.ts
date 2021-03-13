0d6767e3388b16d62e311e2e23fa03286ca68016
Jani Stricelj <9337138+siready@users.noreply.github.com>
2021-03-13 21:15:59 +0100

    Add base queue classes

---
+++ src/infrastructure/broker/queues/base/consumer.base.ts
@@ -0,0 +1,29 @@
+import { Consumer, EachMessagePayload } from "kafkajs";
+
+export abstract class QueueConsumerBase<T> {
+	constructor(private consumer: Consumer, private concurrency: number = 1) {}
+
+	public async connect(): Promise<void> {
+		await this.consumer.connect();
+	}
+
+	public async initialize(): Promise<void> {
+		await this.consumer.subscribe({ topic: this.topic });
+		this.consumer.run({
+			partitionsConsumedConcurrently: this.concurrency,
+			eachMessage: async (payload: EachMessagePayload) => {
+				const value = payload?.message?.value?.toString();
+				if (!value)
+				{
+					throw new Error("Consumed message value is undefined.");
+				}
+				await this.messageProcessor(JSON.parse(value));
+			}
+		})
+		.catch(error => console.error(`Consumer (${this.topic}) error.`, error))
+	}
+
+	protected abstract messageProcessor(message: T): Promise<void>;
+
+	protected abstract get topic(): string;
+}
