03677973a48c6ba84b476538082ffcd02b32c36a
Jani Stricelj <9337138+siready@users.noreply.github.com>
2021-03-13 12:42:24 +0100

    Add validation middleware

---
+++ src/infrastructure/exceptions/validation.exception.ts
@@ -0,0 +1,13 @@
+import { ValidationError } from "class-validator";
+
+export class ValidationException extends Error
+{
+	constructor(validationErrors: ValidationError[]) {
+		super();
+		// Error.captureStackTrace(this, this.constructor)
+
+		this.validationErrors = validationErrors;
+	}
+
+	validationErrors: ValidationError[];
+}
