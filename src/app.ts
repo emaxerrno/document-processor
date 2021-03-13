2d676d40dcf86229993068c2dd2eda18cce0ce65
Jani Stricelj <9337138+siready@users.noreply.github.com>
2021-03-13 21:18:09 +0100

    Add broker initialization

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import DB from './infrastructure/database/database';
 import errorHandlingMiddleware from "./infrastructure/middleware/errorHandling.middleware";
 import notFoundRoute from "./infrastructure/routes/notFound.route";
 import Routes from "./api/routes";
+import Broker from "./infrastructure/broker/broker";
+import { Server } from "node:http";
 
 class App {
 	public readonly app: express.Application;
@@ -15,36 +17,41 @@ class App {
 		this.env = process?.env?.['NODE_ENV'] || 'development';
 	}
 
-	public async initialize() {
-		await this.connectToDatabase();
+	public async initialize(): Promise<void> {
+		await this.initializeDatabase();
+		await this.initializeBroker();
 		this.initializeMiddlewares();
 		this.initializeRoutes();
 		this.initializeErrorHandling();
 	}
 
-	public listen() {
-		this.app.listen(this.port, () => {
+	public listen(): Server {
+		return this.app.listen(this.port, () => {
 			console.log("Server is running on port", this.port);
 		});
 	}
 
-	private async connectToDatabase() {
+	private async initializeDatabase(): Promise<void> {
 		await DB.sequelize.authenticate();
 		// TODO: remove and use migrations
 		await DB.sequelize.sync({ force: false });
 	}
 
-	private initializeMiddlewares() {
+	private async initializeBroker(): Promise<void> {
+		await Broker.initialize();
+	}
+
+	private initializeMiddlewares(): void {
 		this.app.use(express.json());
 		this.app.use(express.urlencoded({ extended: true }));
 	}
 
-	private initializeRoutes() {
+	private initializeRoutes(): void {
 		this.app.use('/api/v1', Routes);
 		this.app.use(notFoundRoute());
 	}
 
-	private initializeErrorHandling() {
+	private initializeErrorHandling(): void {
 		this.app.use(errorHandlingMiddleware());
 	}
 }
