6f606414b095e26eae5dd14b55e2f65825b06a97
Jani Stricelj <9337138+siready@users.noreply.github.com>
2021-03-15 22:20:01 +0100

    Use url hash as partition key

--- src/domain/document/document.queue.service.ts
+++ src/domain/document/document.queue.service.ts
@@ -1,13 +1,13 @@
 import { DocumentRequested } from "../../events/documents/documentRequested.event";
 import { IQueueProducer } from "../../infrastructure/broker/producer.interface";
-import { UuidUtils } from "../../infrastructure/utils/uuid.utils";
+import { HashUtils } from "../../infrastructure/utils/hash.utils";
 
 export class DocumentQueueService {
 	constructor(private documentRequestedProducer: IQueueProducer<DocumentRequested>) {}
 
 	public async sendDocumentRequestedEvent(event: DocumentRequested) {
 		await this.documentRequestedProducer.send(
-			UuidUtils.getUuid().toString(), // TODO: use url hash as partition key
+			HashUtils.generate(event.url),
 			event
 		);
 	}
