ee5c03173c556dfbb9a7af53a50f22e60e33b221
Jani Stricelj <9337138+siready@users.noreply.github.com>
2021-03-17 00:12:24 +0100

    Use problem details response for exceptions

--- src/infrastructure/exceptions/http.exception.ts
+++ src/infrastructure/exceptions/http.exception.ts
@@ -1,12 +1,41 @@
+import { ProblemDocument, ProblemDocumentExtension } from "http-problem-details";
+import { ProblemDocumentOptions } from "http-problem-details/dist/ProblemDocument";
+
 export class HttpException extends Error {
 
-	constructor(status: number, message: string) {
-    super(message);
-    this.status = status;
-    this.message = message;
-  }
+	constructor(status: number, title: string, details?: string, instance?: string) {
+		super(title);
+		this.status = status;
+		this.title = title;
+		this.details = details;
+		this.instance = instance;
+		this.type = `https://httpstatuses.com/${status}`;
+	}
 
 	public readonly status: number;
-  public readonly message: string;
-}
+	public readonly title: string;
+	public readonly details?: string;
+	public readonly instance?: string;
+	public readonly type?: string;
 
+	getProblemDocument(): ProblemDocument {
+		const problemDocumentExtension = this.getProblemDocumentExtension();
+		const problemDocumentOptions: ProblemDocumentOptions = Object.assign({}, {
+			status: this.status,
+			title: this.title,
+			detail: !this.details ? undefined : this.details,
+			instance: !this.instance ? undefined : this.instance,
+			type: !this.type ? undefined : this.type
+		});
+
+		if (problemDocumentExtension !== null) {
+			return new ProblemDocument(problemDocumentOptions, problemDocumentExtension);
+		}
+
+		return new ProblemDocument(problemDocumentOptions);
+	}
+
+	getProblemDocumentExtension(): ProblemDocumentExtension | null {
+		return null;
+	}
+}
--- src/infrastructure/middleware/errorHandling.middleware.ts
+++ src/infrastructure/middleware/errorHandling.middleware.ts
@@ -1,21 +1,18 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ProblemDocument } from "http-problem-details";
 import { HttpException } from "../exceptions/http.exception";
-import { ValidationException } from "../exceptions/validation.exception";
 
 function errorHandlingMiddleware(): ErrorRequestHandler {
 	return (err: Error, _req: Request, res: Response, _next: NextFunction): Response => {
-		if (err instanceof ValidationException) {
-			return res.status(422).json(err);
-		}
 		// TODO: http problem details
 		if (err instanceof HttpException) {
-			return res.status(err.status).send({
-				status: err.status,
-				message: err.message
-			});
+			return res.status(err.status).send(err.getProblemDocument());
 		}
 		// no known error return 500
-		return res.status(500).send();
+		return res.status(500).send(new ProblemDocument({
+			type: `https://httpstatuses.com/500`,
+			status: 500
+		}));
 	}
 }
 
